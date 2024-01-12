9318b1dfcda617f10a49a86488afb8e40b5ecd21
Sarah Ohlin <sarahohlin1@gmail.com>
2024-01-12 02:09:52 -0500

    fix indentation

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,25 @@ import fs from "fs";
 import { config } from "dotenv";
 config();
 
-// 																																		optional \r since that's only on windows
+//															optional \r since that's only on windows
 let responses: string[] = fs.readFileSync("ultraresponses.txt", { encoding: "utf-8" }).split(/\r?\n/g);
 let spamChannel: GuildTextBasedChannel | undefined = undefined;
 const SPAM_INTERVAL = 30000;
 
 const client = new Client({ intents: GatewayIntentBits.Guilds })
 client.on("ready", async (readyClient) => {
-  console.log(`logged in ${readyClient.user.tag}`)
+	console.log(`logged in ${readyClient.user.tag}`)
 
 	// load spam channel
 	spamChannel = await fetchTextBasedChannel(process.env.SPAM_CHANNEL_ID);
 
-  setInterval(() => {
+	setInterval(() => {
 		spamChannel.send(generateUltraResponse());
 	}, SPAM_INTERVAL)
 })
 
 client.on("interactionCreate", async interaction => {
-  if (!interaction.isChatInputCommand()) return
+	if (!interaction.isChatInputCommand()) return
 
 	// handle commands
 	switch (interaction.commandName) {
@@ -44,25 +44,25 @@ client.on("interactionCreate", async interaction => {
 // tries to fetch the channel with the given id. returned undefined if it does not exist or it is not text based. (lol based) 
 async function fetchTextBasedChannel(channel_id: string): Promise<GuildTextBasedChannel | undefined> {
 	return await client.channels.fetch(channel_id, { cache: true }).then(ch => {
-			return ch.isTextBased() ? ch : undefined;
-		}).catch(e => {
-			console.error(`Could not load default spam channel: ${e}`);
-			return undefined;
-		});
+		return ch.isTextBased() ? ch : undefined;
+	}).catch(e => {
+		console.error(`Could not load default spam channel: ${e}`);
+		return undefined;
+	});
 }
 
 function generateUltraResponse(): string {
-  let randomIndex = getRandomIndex();
-  console.log(`random message index: ${randomIndex}, sending message: ${responses[randomIndex]}`);
-  return responses[randomIndex];
+	let randomIndex = getRandomIndex();
+	console.log(`random message index: ${randomIndex}, sending message: ${responses[randomIndex]}`);
+	return responses[randomIndex];
 }
 
 let lastResponseIndex: number;
 function getRandomIndex() {
-  let randomIndex = Math.floor(Math.random() * (responses.length));
-  if (randomIndex == lastResponseIndex) randomIndex = getRandomIndex();
-  lastResponseIndex = randomIndex;
-  return randomIndex;
+	let randomIndex = Math.floor(Math.random() * (responses.length));
+	if (randomIndex == lastResponseIndex) randomIndex = getRandomIndex();
+	lastResponseIndex = randomIndex;
+	return randomIndex;
 }
 
 client.login(process.env.TOKEN!)
