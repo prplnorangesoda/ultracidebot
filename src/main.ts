a020e37e41d3b674203214caa5fe101b981b9f54
Sarah Ohlin <sarahohlin1@gmail.com>
2024-01-12 02:07:44 -0500

    random ass sarah refactor

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,55 @@
-import { Channel, Client,GatewayIntentBits } from "discord.js";
+import { Channel, Client, GatewayIntentBits, GuildTextBasedChannel } from "discord.js";
 import fs from "fs";
 
 import { config } from "dotenv";
 config();
 
-let responses: string[] = fs.readFileSync("ultraresponses.txt",{ encoding: "utf-8" }).split('\r\n');
-let lastResponseIndex: number;
-let ChannelIdToSpam: string = process.env.SPAM_CHANNEL_ID;
+// 																																		optional \r since that's only on windows
+let responses: string[] = fs.readFileSync("ultraresponses.txt", { encoding: "utf-8" }).split(/\r?\n/g);
+let spamChannel: GuildTextBasedChannel | undefined = undefined;
+const SPAM_INTERVAL = 30000;
 
-const client = new Client({intents: GatewayIntentBits.Guilds})
-client.on("ready", readyClient => {
+const client = new Client({ intents: GatewayIntentBits.Guilds })
+client.on("ready", async (readyClient) => {
   console.log(`logged in ${readyClient.user.tag}`)
-  setInterval(periodicallySendUltraMessage, 30000)
-})
 
-async function periodicallySendUltraMessage() {
-  if (!client.isReady()) return;
-  let channel = client.channels.cache.get(ChannelIdToSpam)
-  if(channel === undefined) {
-    console.error("babes, no channel found with id", ChannelIdToSpam)
-    return;
-  }
-  if(channel.isTextBased()) {
-    channel.send(generateUltraResponse())
-  }
-}
+	// load spam channel
+	spamChannel = await fetchTextBasedChannel(process.env.SPAM_CHANNEL_ID);
+
+  setInterval(() => {
+		spamChannel.send(generateUltraResponse());
+	}, SPAM_INTERVAL)
+})
 
 client.on("interactionCreate", async interaction => {
   if (!interaction.isChatInputCommand()) return
 
-  if (interaction.commandName === "getanultrareply") {
-    await interaction.reply(generateUltraResponse())
-  }
-  if (interaction.commandName === "setchannel") {
-    try {
-      ChannelIdToSpam = interaction.channelId;
-      await interaction.reply("set channel successfully")
-    }
-    catch (err) {
-      console.error("error setting new channel id:", err)
-    }
-  }
+	// handle commands
+	switch (interaction.commandName) {
+		case "getanultrareply": {
+			await interaction.reply(generateUltraResponse());
+			break;
+		}
+		case "setchannel": {
+			spamChannel = interaction.channel;
+			await interaction.reply("set channel successfully")
+			break;
+		}
+		default: {
+			break;
+		}
+	}
 })
+
+// tries to fetch the channel with the given id. returned undefined if it does not exist or it is not text based. (lol based) 
+async function fetchTextBasedChannel(channel_id: string): Promise<GuildTextBasedChannel | undefined> {
+	return await client.channels.fetch(channel_id, { cache: true }).then(ch => {
+			return ch.isTextBased() ? ch : undefined;
+		}).catch(e => {
+			console.error(`Could not load default spam channel: ${e}`);
+			return undefined;
+		});
+}
 
 function generateUltraResponse(): string {
   let randomIndex = getRandomIndex();
@@ -49,6 +57,7 @@ function generateUltraResponse(): string {
   return responses[randomIndex];
 }
 
+let lastResponseIndex: number;
 function getRandomIndex() {
   let randomIndex = Math.floor(Math.random() * (responses.length));
   if (randomIndex == lastResponseIndex) randomIndex = getRandomIndex();
